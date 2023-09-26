d9f4def2f3a6b91608cc848659fd24d63f157671
unknown <c0b2207762@edu.teu.ac.jp>
2023-09-26 16:49:19 +0900

    演習2修正2回目

--- pygame_image.py
+++ pygame_image.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bg2_img = pg.transform.flip(bg_img, True, False)
