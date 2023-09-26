ada4ec5402619394af1f48103b8c69524894452d
unknown <c0b2207762@edu.teu.ac.jp>
2023-09-26 15:36:42 +0900

    5番

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,7 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, True)
+    kk_img = pg.transform.flip(kk_img, True, False)
     kk10_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     tmr = 0
     
@@ -19,6 +19,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(bird_img[tmr%2],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
