ef6ac0a602ecdab7b9500cd646d2a7acff67863d
unknown <c0b2207762@edu.teu.ac.jp>
2023-09-26 16:00:55 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -9,11 +9,13 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk5_img = pg.transform.rotozoom(kk_img, 5, 1.0)
     kk10_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk15_img = pg.transform.rotozoom(kk_img, 15, 1.0)
     tmr = 0
     x = 0
     
-    bird_img =[kk_img,kk10_img] 
+    bird_img =[kk_img, kk5_img, kk10_img,kk15_img,kk10_img, kk5_img] 
     
     while True:
         for event in pg.event.get():
@@ -21,11 +23,11 @@ def main():
         
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [1600-x, 0])
-        screen.blit(bird_img[tmr%2], [300,200])
+        screen.blit(bird_img[tmr%4], [300,200])
         pg.display.update()
         x += 1
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1
+        clock.tick(10)
 
 
 if __name__ == "__main__":
